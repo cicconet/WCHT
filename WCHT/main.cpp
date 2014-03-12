3a19c8886f1febd953cec4ce50691392aee69e18
Marcelo Cicconet <cicconet@gmail.com>
2014-03-12 14:49:53 -0400

    ...

---
+++ WCHT/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  WCHT
+//
+//  Created by Marcelo Cicconet on 3/12/14.
+//  Copyright (c) 2014 Marcelo Cicconet. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
